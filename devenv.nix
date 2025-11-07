4ea2ea671eec0efef997de0de57d99d4e88d5a69
Maarten Nijhuis <maarten@qabam.nl>
2025-11-07 11:14:12 +0100

    website content update

--- devenv.nix
+++ devenv.nix
@@ -10,12 +10,8 @@
     pkgs.git
     pkgs.nodejs_20 # Node.js includes npm
     pkgs.nodePackages.serve # Add the 'serve' package for a simple local server
+    pkgs.nodePackages.tailwindcss # Add Tailwind CSS
   ];
-
-  # Commands to run automatically when you enter the devenv shell
-  enterShell = ''
-   build
-  '';
 
   # Define custom scripts (commands) you can run manually
   scripts = {
@@ -26,47 +22,21 @@
       exec = "npm run tailwind:watch"; # Calls the script defined in package.json
     };
     "dev" = {
-      exec = ''
-        echo "Starting dev processes (already running if you just entered shell)."
-        npm run copy:html & # Ensure HTML is copied
-        npm run copy:assets & # Ensure assets are copied
-        npm run tailwind:watch &
-        npx serve ./docs -l 8001 &
-    
-      '';
-      };
-    build.exec = ''    # Ensure npm dependencies are installed
-    npm install
-
-    kill $(lsof -ti :8001)
-
-
-    # Copy index.html to docs folder
-    npm run copy:html
-
-    # Copy all assets to docs/assets folder
-    npm run copy:assets
-
-    # Start Tailwind CSS watcher in the background using the defined script
-    npm run tailwind:watch &
-    npm run tailwind:build &
-
-    # Start the local web server using 'serve' in the background
-    # It will serve files from the 'docs' directory on port 8001
-    npx serve ./docs -l 8001 &
-
-
-    echo "Development server running at http://localhost:8001"
-    echo "Tailwind CSS watcher is active." 
-    '';
-    
+      exec = "npm run copy && npm run tailwind:watch";
+    };
+    "build" = {
+      exec = "npm run copy && npm run tailwind:build";
+    };
   };
 
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
+  processes = {
+    watcher = {
+      exec = "npm run dev";
+    };
+    server = {
+      exec = "serve docs";
+    };
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
